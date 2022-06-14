42b95cf1c910e204d75379b08839eaa370011baf
sravyaganda <90661546+sravyaganda@users.noreply.github.com>
2022-06-14 11:23:56 -0400

    Time: 3 ms (48.58%), Space: 48.5 MB (21.49%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int[] ans=new int[n+1];
+        
+        for(int x=0;x<=n;x++)
+        {
+            ans[x]=popCount(x);
+        }
+        return ans;
+    }
+    
+    public int popCount(int x)
+    {
+        int count;
+        
+        for(count=0;x!=0;count++)
+        {
+            x&=x-1;
+        }
+        
+        return count;
+    }
+}
