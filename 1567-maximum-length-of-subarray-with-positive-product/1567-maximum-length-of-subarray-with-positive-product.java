4930a5ad8b93e404b17e7ac8d1a1e8aac293efe9
sravyaganda <90661546+sravyaganda@users.noreply.github.com>
2022-06-24 11:18:54 -0400

    Time: 6 ms (36.13%), Space: 77.6 MB (67.40%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
@@ -3,24 +3,39 @@ class Solution {
         
         // when ever the product is positive we store the length;
         
-      int neg=-1,zero=-1,negCount=0,ans=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<0){
+     int neg=-1,zero=-1,negCount=0,ans=0;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<0)
+            {
                 negCount++;
-                if(neg==-1) neg=i;
+                if(neg==-1)
+                {
+                    neg=i;
+                }
             }
-            if(nums[i]==0){
+            
+            if(nums[i]==0)
+            {
                 negCount=0;
-                neg=-1;
                 zero=i;
+                neg=-1;
             }
-            else{
-                if(negCount%2==0) 
+            
+            else
+            {
+                if(negCount%2==0)
+                {
                     ans=Math.max(i-zero,ans);
+                }
                 else
+                {
                     ans=Math.max(i-neg,ans);
+                }
             }
         }
+        
         return ans;
         
     }
