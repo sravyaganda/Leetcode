b38540be81036a412761293fc4b15c876c965d2f
sravyaganda <90661546+sravyaganda@users.noreply.github.com>
2022-06-24 11:06:38 -0400

    Time: 7 ms (24.64%), Space: 78.6 MB (48.96%) - LeetHub

---
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int getMaxLen(int[] nums) {
+        
+        // when ever the product is positive we store the length;
+        
+      int neg=-1,zero=-1,negCount=0,ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0){
+                negCount++;
+                if(neg==-1) neg=i;
+            }
+            if(nums[i]==0){
+                negCount=0;
+                neg=-1;
+                zero=i;
+            }
+            else{
+                if(negCount%2==0) 
+                    ans=Math.max(i-zero,ans);
+                else
+                    ans=Math.max(i-neg,ans);
+            }
+        }
+        return ans;
+        
+    }
+}
