c4d9c77c41c17c021c234bf1ac37a057d546264c
sravyaganda <90661546+sravyaganda@users.noreply.github.com>
2022-07-05 14:12:19 -0400

    Time: 110 ms (43.41%), Space: 69.5 MB (44.53%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
@@ -1,35 +1,23 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
       
-        Stack<Integer> stack=new Stack();
-        int[] previousLess=new int[arr.length];
-        int[] nextLess=new int[arr.length];
-        Arrays.fill(nextLess,arr.length);
+        Stack<Integer> stack = new Stack<>();
+        int n = arr.length;
+        long count = 0;
+        double mod=(1e9 +7);
         
-        for(int i=0;i<arr.length;i++)
-        {
-            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i])
-            {
-                nextLess[stack.pop()]=i;
+        for(int i=0;i<=n;i++){
+            while(!stack.isEmpty() && (i==n || arr[stack.peek()]>arr[i])){
+                int mid = stack.pop();
+                int l = mid - (stack.isEmpty() ? -1 : stack.peek());
+                int r = i - mid;
+                
+                count += (long)arr[mid]*(l)*(r);
+                count%=mod;
             }
-            
-            previousLess[i]=stack.isEmpty() ? -1:stack.peek();
-            
-            stack.push(i);            
+            stack.push(i);
         }
         
-        long sum=0;
-        double mod=1e9 +7;
-        
-        for(int i=0;i<arr.length;i++)
-        {
-            long left=i-previousLess[i];
-            long right=nextLess[i]-i;
-            
-            sum+=arr[i]*(left*right);
-            sum%=mod;
-        }
-        
-        return (int)sum;
+        return (int)(count);
     }
 }
