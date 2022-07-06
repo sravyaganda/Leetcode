3def80bd9ac6795e28628d2bac2306084845bf8f
sravyaganda <90661546+sravyaganda@users.noreply.github.com>
2022-07-06 09:14:23 -0400

    Time: 50 ms (41.94%), Space: 48.6 MB (40.10%) - LeetHub

---
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
+    
+        Map<String,List<Pair<Integer,String>>> usersMap=new HashMap();
+        
+        
+        for(int i=0;i<timestamp.length;i++)
+        {
+            String user=username[i];       
+            List<Pair<Integer,String>> visits=usersMap.getOrDefault(user,new ArrayList<Pair<Integer,String>>());
+            visits.add(new Pair<Integer,String>(timestamp[i],website[i]));
+            usersMap.put(user,visits);
+        }
+        
+        Map<String,Set<String>> patternMap=new HashMap();
+        String result="";
+        int maxCount=0;
+        
+        for(Map.Entry<String,List<Pair<Integer,String>>> entry: usersMap.entrySet())
+        {
+            String user=entry.getKey();
+            List<Pair<Integer,String>> visits=entry.getValue();
+            Collections.sort(visits,(a,b)-> a.getKey()-b.getKey());
+            
+            for(int i=0;i<visits.size()-2;i++)
+            {
+                for(int j=i+1;j<visits.size()-1;j++)
+                {
+                    for(int k=j+1;k<visits.size();k++)
+                    {
+                        String pattern=visits.get(i).getValue() +":"+
+                            visits.get(j).getValue()+":"+
+                            visits.get(k).getValue();
+                        Set<String> usersSet=patternMap.getOrDefault(pattern,new HashSet<String>());
+                        usersSet.add(user);
+                        
+                        patternMap.put(pattern,usersSet);
+                        if(patternMap.get(pattern).size()>maxCount)
+                        {
+                            maxCount=patternMap.get(pattern).size();
+                            result=pattern;
+                        }
+                        else if(patternMap.get(pattern).size()==maxCount)
+                        {
+                            result= result.compareTo(pattern)<=0?result:pattern; 
+                        }
+                    }
+                }
+            }
+       
+        }
+        
+             
+            return Arrays.asList(result.split(":"));
+    }
+}
