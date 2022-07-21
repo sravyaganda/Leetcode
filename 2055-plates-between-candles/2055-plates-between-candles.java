beba0a36afd353b0318f7073207b54cc0cf3c69f
sravyaganda <90661546+sravyaganda@users.noreply.github.com>
2022-07-21 18:02:35 -0400

    Time: 2414 ms (5.09%), Space: 94.6 MB (94.79%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.java
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -2,30 +2,38 @@ class Solution {
     public int[] platesBetweenCandles(String s, int[][] queries) {
         
        int n = s.length();
-    //calculate sum so far array : *-> add,  |-> keep prev
+  
     int[] total = new int[n];
     total[0] = s.charAt(0)=='*' ? 1 : 0;
+          System.out.print(0 +"-"+total[0]+"  ");
     for(int i = 1; i<n; i++) 
-        total[i] = s.charAt(i)=='*' ? total[i-1]+1 : total[i-1];
+    {  total[i] = s.charAt(i)=='*' ? total[i-1]+1 : total[i-1];
+        System.out.print(i +"-"+total[i]+"  ");}
     
-    //calculate left : index of left star 
+   System.out.println();
     int[] left = new int[n];
     left[0] = s.charAt(0)=='|' ? 0 : -1;
+    System.out.print(0 +"-"+left[0]+"  ");
     for(int i = 1; i<n; i++) 
-        left[i] = s.charAt(i)=='|' ? i : left[i-1];
+    {  left[i] = s.charAt(i)=='|' ? i : left[i-1];
+        System.out.print(i +"-"+left[i]+"  ");
+    }
     
-    //calculate right : index of right star 
-    int[] rigth = new int[n];
-    rigth[n-1] = s.charAt(0)=='|' ? n-1 : -1;
+       
+   System.out.println();
+    int[] right = new int[n];
+    right[n-1] = s.charAt(n-1)=='|' ? n-1 : -1;
+      //  System.out.print(9 +"-"+right[9]+"  ");
     for(int i = n-2; i>=0; i--) 
-        rigth[i] = s.charAt(i)=='|' ? i : rigth[i+1];
+    { right[i] = s.charAt(i)=='|' ? i : right[i+1];
+     System.out.print(i +"-"+right[i]+"  ");
+    }
     
-    //for each query based on left and rigth get a close rang and then do prefix sum (sum(j)-sum(i))
-    // if there is no closed range, then add 0 as answer
+    
     int[] ans = new int[queries.length];
     int i = 0;
     for(int[] q:queries) {
-        int start = rigth[q[0]];
+        int start = right[q[0]];
         int end = left[q[1]];            
         if(start==-1 || end==-1 || start>end) 
             ans[i++] = 0;
